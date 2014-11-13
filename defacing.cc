259da52396905d06cef22cc2cffb2cd15e5d2fe2
Manoj <janmanoj@gmail.com>
2014-11-13 17:21:51 -0500

    boost

--- defacing.cc
+++ defacing.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 
 const int compatibility [10][10] = {
     1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
@@ -15,56 +16,94 @@ const int compatibility [10][10] = {
     0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 1, 1
 };
-int max_possible_val (int index, const std::string& score, const std::string& max)
+
+std::string max_possible_val (int index, const std::string& score, const std::string& max)
 {
-    int number = stoi(max[index]);
-    std::cout<<"Int val: "<< max_int<<std::endl;
+    using boost::lexical_cast;
+
+    int max_val  = lexical_cast<int>(max[index]);
+    int score_val = lexical_cast<int> (score[index]);
     if(score[index] == '~') {
-        return (number - 1);
+        return lexical_cast<std::string> (max_val - 1);
     }
 
-    for(int i = (number - 1) ; i >= 0 ; i--) {
+    for(int i = max_val ; i > 0 ; i--) {
 
-        if(compatibility[number][i])
-            return i;
+        if(compatibility[score_val][i]) {
+            //std::cout<<"Returning " << i << "\n";
+            return lexical_cast<std::string>(i);
+        }
     }
 
 }
+
 bool compatible(const std::string& score, const std::string& max)
 {
+    using boost::lexical_cast;
+
     std::string temp = score;
     int score_int, max_int;
-    boost::replace_all(temp, "~", "9");
-    score_int = stoi(temp);
-    max_int = stoi(max);
+    for(int i = 0; i <temp.length(); i++) {
+        if(temp[i] == '~')
+            temp[i] = max[i];
+    }
+    score_int = lexical_cast<int>(temp);
+    max_int = lexical_cast<int> (max);
     if (score_int <= max_int)
         return true;
     return false;
 }
 
-void maximize (std::string& score, const std::string& max)
+void colour (std::string& score, const std::string& max)
 {
-    score[0] = max[0];
-    if(!compatible(score,max)) {
-        score[0] = max_possible_val(0, score, max);
+    using boost::lexical_cast;
+
+    for(int i=0; i < score.length() ; i++) {
+        int score_digit = lexical_cast<int> (score[i]);
+        int max_digit = lexical_cast<int> (max[i]);
+        for(int j= max_digit; j > score_digit; j--) {
+            if(compatibility[score_digit][j]) {
+                score.replace(i, 1, lexical_cast<std::string>(j));
+                break;
+		}
+        }
     }
-    for(int i = 1; i < score.length(); i++) {
-        score[i] = max_possible_val(i, score, max);
-        std::cout<<"Well: "<<score[i]<<std::endl;
+
+}
+
+void maximize (bool padded, std::string& score, const std::string& max)
+{
+    using boost::lexical_cast;
+
+    if(padded) {
+        for(int i = 0; i < score.length() ; i++) {
+            if(score[i] == '~')
+                score[i] = max[i];
+            if(!compatible(score,max)) {
+                std::string retval;
+                retval = max_possible_val(i, score, max);
+                score.replace(i, 1, retval);
+            }
+        }
     }
-    std::cout << score <<"\n";
+    else {
+        colour(score, max);
+    }
+    std::cout << lexical_cast<int>(score) <<"\n";
 }
 
 int main(void)
 {
     int testcases = 1;
+    bool padded = false;
     //std::cin>>testcases;
     while(testcases--) {
         std::string score, max_score;
         std::cin>> score >> max_score;
         while(score.length() <  max_score.length()) {
             score.insert(0,"~");
+            padded = true;
         }
-        maximize(score, max_score);
+        maximize(padded, score, max_score);
     }
 }
